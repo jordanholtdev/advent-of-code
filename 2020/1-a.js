f973c560a9177879919bce109170350731ed4426
J Holt <jordan.su@protonmail.com>
2020-12-02 18:03:52 -0500

    solution to day 1 puzzle 1 (1-a)

---
+++ 2020/1-a.js
@@ -0,0 +1,25 @@
+const readFile = require('fs').readFileSync;
+
+// prep the input
+const file = readFile(__dirname + '/1-a.input', 'utf-8')
+  .split(/\r?\n/)
+  .filter((_) => _.trim())
+  .map((_) => {
+    return parseInt(_, 10);
+  });
+
+// const length = file.length;
+
+let a = null;
+
+let b = file.find((number) => {
+  for (let i = 0; i < file.length; i++) {
+    const m = file[i];
+    if (number + m === 2020) {
+      a = m;
+      return true;
+    }
+  }
+});
+
+console.log(a, b, a + b === 2020, a * b);
